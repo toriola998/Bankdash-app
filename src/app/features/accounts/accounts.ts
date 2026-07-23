88ea3a5936506abbfc5b19f238a42b937d0fb6e3
Toriola <faaedah@gmail.com>
2026-07-23 12:24:40 +0100

    CREATE: created last transactions table

--- src/app/features/accounts/accounts.ts
+++ src/app/features/accounts/accounts.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { PageLayout } from '../../core/layouts/page-layout/page-layout';
+import { CardDetails } from '../../shared/ui/card-details';
+import { LastTransaction } from './components/last-transaction';
 
 interface analytics {
    id: number;
@@ -10,10 +12,10 @@ interface analytics {
 }
 @Component({
    selector: 'app-accounts',
-   imports: [PageLayout],
+   imports: [PageLayout, CardDetails, LastTransaction],
    template: `
       <app-page-layout>
-         <div class="flex flex-col gap-7.5 sm:grid grid-cols-2 lg:grid-cols-4">
+         <div class="flex flex-col gap-7.5 sm:grid grid-cols-2 xl:grid-cols-4">
             @for (item of accountanalytics; track item.id) {
                <div class="card py-6 px-9 flex-items gap-x-7.5">
                   <div
@@ -29,6 +31,28 @@ interface analytics {
                   </div>
                </div>
             }
+         </div>
+
+         <div
+            class="md:grid grid-cols-[50%_auto] xl:grid-cols-[65%_auto] mt-5 gap-7.5">
+            <section>
+               <h2 class="page-title mb-5">Last Transaction</h2>
+
+               <div class="card">
+                  <app-last-transaction></app-last-transaction>
+               </div>
+            </section>
+
+            <section>
+               <div class="flex-between mb-5 mt-5 lg:mt-0">
+                  <h2 class="page-title">My Cards</h2>
+                  <a class="font-semibold text-lg">See all</a>
+               </div>
+
+               <app-card-details
+                  variant="blue"
+                  [cardDetails]="cardDetails"></app-card-details>
+            </section>
          </div>
       </app-page-layout>
    `,
@@ -65,4 +89,12 @@ export class Accounts {
          icontheme: 'bg-green-1',
       },
    ];
+
+   cardDetails = {
+      date: '12/22',
+      name: 'Hola',
+      balance: '$12.00',
+      cardUrl: '/icons/chip-card.svg',
+      masterCardUrl: '/icons/mastercard-white.svg',
+   };
 }
---
+++ src/app/features/accounts/components/last-transaction.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-last-transaction',
+   imports: [],
+   template: `<div
+      class="overflow-x-scroll max-w-full rounded-2xl last-transaction-wrapper">
+      <table class="w-[700px] xl:w-full relative">
+         <thead class="sr-only">
+            <tr>
+               <th class="pl-5"></th>
+               <th>Description</th>
+               <th>Type</th>
+               <th>Card</th>
+               <th>Status</th>
+               <th>Amount</th>
+            </tr>
+         </thead>
+         <tbody class="w-full">
+            <tr class="table-row">
+               <td class="pl-5">
+                  <span class="flex-center rounded-[20px] h-17.5 w-17.5">
+                     <img src="" alt="" />
+                  </span>
+               </td>
+               <td>
+                  <p class="text-black-4 font-semibold">Spotify Subscription</p>
+                  <p class="text-blue-1 text-[15px]">25 Jan 2021</p>
+               </td>
+               <td class="text-blue-1">Shopping</td>
+               <td class="text-blue-1">1234 ****</td>
+
+               <!-- /* [class]="
+                        item.trnxType === 'credit' ? 'text-green' : 'text-red'
+                     " */ -->
+               <td class="pr-8 text-green">-$2350</td>
+            </tr>
+         </tbody>
+      </table>
+   </div>`,
+   styles: `
+      .last-transaction-wrapper::-webkit-scrollbar {
+         display: none;
+      }
+   `,
+})
+export class LastTransaction {}
