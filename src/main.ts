0f9a512d26af1ec2ddb96a4c86efd412f5139919
Toriola <faaedah@gmail.com>
2026-07-29 00:22:43 +0100

    CREATE: installed & configured firebase

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+import { environment as firebaseConfig } from './environments/environment.development';
+
+const firebaseApp = initializeApp(firebaseConfig);
+if (typeof window !== 'undefined') {
+   getAnalytics(firebaseApp);
+}
+
 bootstrapApplication(App, appConfig).catch(err => console.error(err));
