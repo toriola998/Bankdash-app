312db8a835ae9a9eace0cf2a90686b17f9c7230c
Toriola <faaedah@gmail.com>
2026-07-29 23:58:50 +0100

    ADD: added toast service

---
+++ src/app/core/services/toast-service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import Toastify from 'toastify-js';
+// @ts-ignore
+import 'toastify-js/src/toastify.css';
+
+@Injectable({
+   providedIn: 'root',
+})
+export class ToastService {
+   private toast(message: string, background: string) {
+      Toastify({
+         text: message,
+         duration: 10000,
+         gravity: 'top',
+         position: 'right',
+         close: false,
+         stopOnFocus: true,
+         style: {
+            background,
+            'padding': '18px',
+            'border-radius': '4px',
+            'box-shadow':
+               '0 1px 10px 0 rgb(0, 0, 0, 0.1), 0 2px 15px 0 rgb(0, 0, 0, 0.05)',
+         },
+      }).showToast();
+   }
+
+   success(message: string) {
+      this.toast(message, '#07bc0c');
+   }
+
+   error(message: string) {
+      this.toast(message, '#e74c3c');
+   }
+
+   warning(message: string) {
+      this.toast(message, '#f1c40f');
+   }
+
+   info(message: string) {
+      this.toast(message, '#3498db');
+   }
+}
