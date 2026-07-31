5b98812eab92226953581d1060fa114d54da7c93
Toriola <faaedah@gmail.com>
2026-07-31 22:20:14 +0100

    ADD: pushed firebase keys

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+   apiKey: 'AIzaSyBwF37Eo0gngoaKd7ZwkO7zIhAvC0zwJsQ',
+   authDomain: 'bankdash-9a512.firebaseapp.com',
+   projectId: 'bankdash-9a512',
+   storageBucket: 'bankdash-9a512.firebasestorage.app',
+   messagingSenderId: '993334621123',
+   appId: '1:993334621123:web:cc2a4fcf040f5494e0c376',
+   measurementId: 'G-9NEZ84YRKF',
+};
